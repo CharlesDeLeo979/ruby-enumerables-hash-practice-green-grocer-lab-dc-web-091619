f0ea5504ece36aedcfd19104e6d9dddecb7a0f92
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-09 21:16:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,13 +19,21 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
+<<<<<<< HEAD
     if cart.has_key?(item) 
       if !cart.has_key?("#{item} W/COUPON") && cart[item][:count] >= coupon[:num] 
+=======
+    if cart[item] 
+      if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
+>>>>>>> 02a0b12cc2f702a7ddcb749ae0e48df430c6edda
         cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
         cart[item][:count] -= coupon[:num]
     elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
           cart["#{item} W/COUPON"][:count] += coupon[:num]
+<<<<<<< HEAD
                cart[item][:count] -= coupon[:num]
+=======
+>>>>>>> 02a0b12cc2f702a7ddcb749ae0e48df430c6edda
      end
     end
   end
@@ -34,15 +42,27 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name, stats|
+<<<<<<< HEAD
     stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
+=======
+    stats[:price] -= stats[:price] * 0.2 unless !stats[:clearance]
+>>>>>>> 02a0b12cc2f702a7ddcb749ae0e48df430c6edda
   end
   cart
 end
 
 def checkout(cart, coupons)
+<<<<<<< HEAD
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
   total > 100 ? total * 0.9 : total
+=======
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
+  total > 100 ?
+>>>>>>> 02a0b12cc2f702a7ddcb749ae0e48df430c6edda
 end
